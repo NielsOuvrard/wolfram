2fa4c5351c8b5f476a15263054872546a0b7d508
Grafox <niels.ouvrard@epitech.eu>
2023-03-02 15:36:55 +0100

    header

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,9 @@
+{-
+-- EPITECH PROJECT, 2023
+-- rush 1
+-- File description:
+-- it's gonna be sorted
+-}
 module Main (main) where
 
 import Lib
@@ -16,3 +22,6 @@ main = do
     -- main = someFunc
     print y
     putStrLn "OK" >> exitWith (ExitSuccess)
+
+-- B-FUN-400-MAR-4-1-wolfram-niels.ouvrard/app/Main.hs:1: MAJOR:H-G1 # B-FUN-400-MAR-4-1-wolfram-niels.ouvrard/app/Main.hs has a badly formatted Epitech header
+-- B-FUN-400-MAR-4-1-wolfram-niels.ouvrard/src/Lib.hs:1: MAJOR:H-G1 # B-FUN-400-MAR-4-1-wolfram-niels.ouvrard/src/Lib.hs has a badly formatted Epitech header
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,12 @@
-module Lib
-    ( someFunc
-    ) where
 {-
 -- EPITECH PROJECT, 2023
 -- rush 1
 -- File description:
 -- it's gonna be sorted
 -}
+module Lib
+    ( someFunc
+    ) where
 
 import System.Environment
 import System.Exit
