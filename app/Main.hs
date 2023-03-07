855b835e5e687a12abc5e10ce891f526452318cc
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-03-07 23:05:33 +0100

    arguments gestion

--- app/Main.hs
+++ app/Main.hs
@@ -10,65 +10,67 @@ import Lib()
 
 import System.Environment
 import System.Exit
-import Data.Char( isDigit )
-
--- print :: Show a => a -> IO ()
--- print x = putStrLn (show x)
+-- import Data.Char
 
 data Conf = Conf {
-    rule    :: Int, --
-    start   :: Int, -- devault 0
-    -- lines   :: Int, -- devault never stop
-    window  :: Int, -- devault 80
-    move    :: Int -- if positive, move to the right, if negative, move to the left
+    rule     :: Int, --
+    start    :: Int, -- devault 0
+    thelines :: Int, -- devault never stop
+    window   :: Int, -- devault 80
+    move     :: Int -- if positive, move to the right, if negative, move to the left
 }
 
 defaultConf :: Conf
 defaultConf = Conf {
-    rule = 0,
+    rule = (-1),
     start = 0,
-    -- lines = 0,
+    thelines = 0,
     window = 80,
     move = 0
 }
 
-getOpts :: Conf -> [ String ] -> Maybe Conf
-getOpts conf [] = Just conf
-getOpts conf (x:xs) = case x of
-    "-rule" -> getOpts (conf { rule = read (head xs) }) (tail xs)
-    "-start" -> getOpts (conf { start = read (head xs) }) (tail xs)
-    -- "-lines" -> getOpts (conf { lines = read (head xs) }) (tail xs)
-    "-window" -> getOpts (conf { window = read (head xs) }) (tail xs)
-    "-move" -> getOpts (conf { move = read (head xs) }) (tail xs)
-    _ -> Nothing
-
-showConf :: Conf -> IO ()
-showConf conf = do
+showConf :: Maybe Conf -> IO ()
+showConf Nothing = exitWith (ExitFailure 84)
+showConf (Just conf) = do
     print (rule conf)
     print (start conf)
-    -- print (lines conf)
+    print (thelines conf)
     print (window conf)
     print (move conf)
 
+modifyConf :: Maybe Conf -> String -> Int -> Maybe Conf
+modifyConf Nothing x y = modifyConf (Just defaultConf) x y
+modifyConf (Just conf) "-rule" x = Just conf { rule = x }
+modifyConf (Just conf) "-start" x = Just conf { start = x }
+modifyConf (Just conf) "-lines" x = Just conf { thelines = x }
+modifyConf (Just conf) "-window" x = Just conf { window = x }
+modifyConf (Just conf) "-move" x = Just conf { move = x }
+modifyConf _ _ _ = Nothing
 
-fromArgsToCompute :: [ String ] -> IO ()
-fromArgsToCompute [] = putStrLn "OK" >> exitWith (ExitSuccess)
-fromArgsToCompute (x:xs) = case x of
-    "-rule" -> putStrLn "OK" >> exitWith (ExitSuccess)
-    "-start" -> putStrLn "OK" >> exitWith (ExitSuccess)
-    -- "-lines" -> putStrLn "OK" >> exitWith (ExitSuccess)
-    "-window" -> putStrLn "OK" >> exitWith (ExitSuccess)
-    "-move" -> putStrLn "OK" >> exitWith (ExitSuccess)
-    _ -> putStrLn "OK" >> exitWith (ExitSuccess)
+fromArgsToConf :: [ String ] -> Maybe Conf -> IO (Maybe Conf)
+fromArgsToConf [] Nothing = exitWith (ExitFailure 84)
+fromArgsToConf x Nothing = fromArgsToConf x (Just defaultConf)
+fromArgsToConf [] (Just conf) = return (Just conf)
+fromArgsToConf (x:(y:ys)) (Just conf) = fromArgsToConf ys (modifyConf (Just conf) x (read y))
+fromArgsToConf _ _ = exitWith (ExitFailure 84)
+
+checkConf :: Maybe Conf -> IO ()
+checkConf Nothing = exitWith (ExitFailure 84)
+checkConf (Just conf) = if rule conf < 1 then exitWith (ExitFailure 84) else return ()
+
 
 main :: IO ()
 main = do
     y <- getArgs
-    fromArgsToCompute y
-    -- showConf Just (getOpts defaultConf y)
-    -- main = someFunc
-    -- print y
-    putStrLn "OK" >> exitWith (ExitSuccess)
+    conf <- fromArgsToConf y Nothing
+    checkConf conf
+    showConf conf
 
--- B-FUN-400-MAR-4-1-wolfram-niels.ouvrard/app/Main.hs:1: MAJOR:H-G1 # B-FUN-400-MAR-4-1-wolfram-niels.ouvrard/app/Main.hs has a badly formatted Epitech header
--- B-FUN-400-MAR-4-1-wolfram-niels.ouvrard/src/Lib.hs:1: MAJOR:H-G1 # B-FUN-400-MAR-4-1-wolfram-niels.ouvrard/src/Lib.hs has a badly formatted Epitech header
+
+
+-- fromArgsToConf ("-rule":xs) (Just conf) = case xs of
+--     [] -> exitWith (ExitFailure 84)
+--     (x:_) -> if isNumber (head x) then fromArgsToConf (tail xs) (Just (conf { rule = read x })) else exitWith (ExitFailure 84)
+-- fromArgsToConf ("-start":xs) (Just conf) = case xs of
+--     [] -> exitWith (ExitFailure 84)
+--     (x:_) -> if isNumber (head x) then fromArgsToConf (tail xs) (Just (conf { start = read x })) else exitWith (ExitFailure 84)
