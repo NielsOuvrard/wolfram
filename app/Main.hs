ae12416fcc70b00cc84ccd0802514e56590439af
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-03-09 18:59:19 +0100

    codding style and code clean

--- app/Main.hs
+++ app/Main.hs
@@ -55,30 +55,51 @@ fromArgsToConf (x:(y:ys)) (Just conf) =
     fromArgsToConf ys (modifyConf (Just conf) x (read y))
 fromArgsToConf _ _ = exitWith (ExitFailure 84)
 
-checkConf :: Maybe Conf -> IO ()
-checkConf Nothing = exitWith (ExitFailure 84)
-checkConf (Just conf) =
-    if rule conf < 1 then exitWith (ExitFailure 84) else return ()
-checkConf (Just conf) =
-    if rule conf > 255 then exitWith (ExitFailure 84) else return ()
-checkConf (Just conf) =
-    if start conf < 0 then exitWith (ExitFailure 84) else return ()
-checkConf (Just conf) =
-    if thelines conf < (-1) then exitWith (ExitFailure 84) else return ()
-checkConf (Just conf) =
-    if window conf < 1 then exitWith (ExitFailure 84) else return ()
+-- checkConf :: Conf -> IO ()
+-- checkConf (Conf rule start thelines window move) = if rule < 1 then exitWith (ExitFailure 84) else return ()
+-- checkConf (Conf rule start thelines window move) = if rule > 255 then exitWith (ExitFailure 84) else return ()
+-- checkConf (Conf rule start thelines window move) = if start < 0 then exitWith (ExitFailure 84) else return ()
+-- checkConf (Conf rule start thelines window move) = if thelines < (-1) then exitWith (ExitFailure 84) else return ()
+-- checkConf (Conf rule start thelines window move) = if window < 1 then exitWith (ExitFailure 84) else return ()
+
+checkConf :: Conf -> IO ()
+checkConf (Conf rule start thelines window move)
+  | rule < 1 || rule > 255 || start < 0 || thelines < (-1) || window < 1 =
+    exitWith (ExitFailure 84)
+  | otherwise = return ()
 
 fromJust :: Maybe Conf -> Conf
 fromJust Nothing = defaultConf
 fromJust (Just conf) = conf
 
+-- conf
+    -- rule     :: Int,
+    -- start    :: Int,
+    -- thelines :: Int,
+    -- window   :: Int,
+    -- move     :: Int
+
+checkConfNotNull :: Maybe Conf -> IO ()
+checkConfNotNull Nothing = exitWith (ExitFailure 84)
+checkConfNotNull (Just conf) = return ()
+
+-- checkConf :: Conf -> IO ()
+-- checkConf (Conf (rule < 1) start thelines window move) = exitWith (ExitFailure 84)
+-- checkConf _ = return ()
+    --      if rule < 1 then exitWith (ExitFailure 84)
+    -- else if rule > 255 then exitWith (ExitFailure 84)
+    -- else if start < 0 then exitWith (ExitFailure 84)
+    -- else if thelines < (-1) then exitWith (ExitFailure 84)
+    -- else if window < 1 then exitWith (ExitFailure 84)
+    -- else return ()
 decimalToBinary :: Int -> [ Int ]
 decimalToBinary 0 = [ 0 ]
 decimalToBinary 1 = [ 1 ]
 decimalToBinary x = decimalToBinary (x `div` 2) ++ [ x `mod` 2 ]
 
 decimalToByte :: Int -> [ Int ]
-decimalToByte x = replicate (8 - length (decimalToBinary x)) 0 ++ decimalToBinary x
+decimalToByte x = replicate (8 - length (decimalToBinary x))
+    0 ++ decimalToBinary x
 
 
 createData :: Conf -> Data
@@ -92,35 +113,42 @@ createData conf = Data {
 }
 
 createStringBeginningEven :: Int -> String
-createStringBeginningEven x = replicate ((x `div` 2) - 1) ' ' ++ replicate 1 '*' ++ replicate ((x `div` 2) - 1) ' '
+createStringBeginningEven x = replicate ((x `div` 2) - 1) ' ' ++
+    replicate 1 '*' ++ replicate ((x `div` 2) - 1) ' '
 
 createStringBeginningOdd :: Int -> String
-createStringBeginningOdd x = replicate (x `div` 2) ' ' ++ replicate 1 '*' ++ replicate ((x `div` 2) - 1) ' '
+createStringBeginningOdd x = replicate (x `div` 2) ' ' ++ replicate 1
+    '*' ++ replicate ((x `div` 2) - 1) ' '
 
 createStringBeginning :: Int -> String
-createStringBeginning x = if x `mod` 2 == 0 then createStringBeginningEven x else createStringBeginningOdd x
+createStringBeginning x =
+    if x `mod` 2 == 0 then createStringBeginningEven x
+    else createStringBeginningOdd x
 
 
 stringTostringPlus2 :: String -> String
 stringTostringPlus2 x = "  " ++ x ++ "  "
 
-createNextChar :: String -> String -> Int -> [ Int ] -> String
-createNextChar line nextLine idx rule = (nextLine ++ [
-         if (line !! idx == ' ' && line !! (idx + 1) == ' ' && line !! (idx + 2) == ' ') then if (rule !! 7 == 1) then '*' else ' '
-    else if (line !! idx == ' ' && line !! (idx + 1) == ' ' && line !! (idx + 2) == '*') then if (rule !! 6 == 1) then '*' else ' '
-    else if (line !! idx == ' ' && line !! (idx + 1) == '*' && line !! (idx + 2) == ' ') then if (rule !! 5 == 1) then '*' else ' '
-    else if (line !! idx == ' ' && line !! (idx + 1) == '*' && line !! (idx + 2) == '*') then if (rule !! 4 == 1) then '*' else ' '
-    else if (line !! idx == '*' && line !! (idx + 1) == ' ' && line !! (idx + 2) == ' ') then if (rule !! 3 == 1) then '*' else ' '
-    else if (line !! idx == '*' && line !! (idx + 1) == ' ' && line !! (idx + 2) == '*') then if (rule !! 2 == 1) then '*' else ' '
-    else if (line !! idx == '*' && line !! (idx + 1) == '*' && line !! (idx + 2) == ' ') then if (rule !! 1 == 1) then '*' else ' '
-    else if (line !! idx == '*' && line !! (idx + 1) == '*' && line !! (idx + 2) == '*') then if (rule !! 0 == 1) then '*' else ' '
-    else ' '
-    ])
+createNextChar :: String -> String -> Int -> [Int] -> String
+createNextChar line nextLine idx rule = nextLine ++
+    [checkThis3Chars (take 3 (drop idx line)) rule]
+
+checkThis3Chars :: String -> [ Int ] -> Char
+checkThis3Chars "   " rule = if rule !! 7 == 1 then '*' else ' '
+checkThis3Chars "  *" rule = if rule !! 6 == 1 then '*' else ' '
+checkThis3Chars " * " rule = if rule !! 5 == 1 then '*' else ' '
+checkThis3Chars " **" rule = if rule !! 4 == 1 then '*' else ' '
+checkThis3Chars "*  " rule = if rule !! 3 == 1 then '*' else ' '
+checkThis3Chars "* *" rule = if rule !! 2 == 1 then '*' else ' '
+checkThis3Chars "** " rule = if rule !! 1 == 1 then '*' else ' '
+checkThis3Chars "***" rule = if rule !! 0 == 1 then '*' else ' '
+checkThis3Chars _ _ = ' '
 
 createNextLine :: String -> String -> Int -> [ Int ] -> String
 createNextLine line nextLine idx binaryRule =
     if idx == length line - 2 then stringTostringPlus2 nextLine
-    else createNextLine line (createNextChar line nextLine idx binaryRule) (idx + 1) binaryRule
+    else createNextLine line (createNextChar line nextLine idx binaryRule)
+    (idx + 1) binaryRule
 
 -- conf
     -- rule     :: Int,
@@ -138,7 +166,8 @@ createNextLine line nextLine idx binaryRule =
     -- generation :: Int
 
 putCenterStr :: String -> Int -> Int -> IO ()
-putCenterStr x size y = if (length x) - (2 * size) == y then putChar '\n' >> return ()
+putCenterStr x size y =
+    if (length x) - (2 * size) == y then putChar '\n' >> return ()
     else putChar (x !! (size + y)) >> putCenterStr x size (y + 1)
 
 
@@ -146,14 +175,14 @@ wolframe :: Data -> IO ()
 wolframe (Data binaryRule conf line nextLine idx generation) =
     if generation == thelines conf then
         exitWith (ExitSuccess)
-    else
-        do
-        putCenterStr line generation 0
-        wolframe (Data binaryRule conf (createNextLine line "" 0 binaryRule) "" 0 (generation + 1))
+    else putCenterStr line generation 0 >>
+        wolframe (Data binaryRule conf
+        (createNextLine line "" 0 binaryRule) "" 0 (generation + 1))
 
 main :: IO ()
 main = do
     y <- getArgs
     conf <- fromArgsToConf y Nothing
-    checkConf conf
+    checkConfNotNull conf
+    checkConf (fromJust conf)
     wolframe (createData (fromJust conf))
